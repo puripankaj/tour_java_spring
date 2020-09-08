4f49dbb4deb82f43c3fbc7291b9b9786896b5486
purip <puripankaj@hotmail.com>
2020-09-07 23:45:27 -0500

    Initial commit

---
+++ src/main/java/com/food/bash/spring/FoodBashSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.food.bash.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBashSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodBashSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/bash/spring/FoodBashSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.bash.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBashSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
