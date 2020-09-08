784d86351f94779e2711e541d90d54daa1fc5a5e
purip <puripankaj@hotmail.com>
2020-09-07 23:54:48 -0500

    Updated application file

--- src/main/java/com/food/bash/spring/FoodBashSpringBootApplication.java
+++ src/main/java/com/food/bash/spring/FoodBashSpringBootApplication.java
@@ -2,10 +2,21 @@ package com.food.bash.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@Controller
 @SpringBootApplication
 public class FoodBashSpringBootApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	   String home() {
+	      return "Welcome FoodBash!";
+	   }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FoodBashSpringBootApplication.class, args);
 	}
